2350eeeebf6b85475defdfec5717c700f30b75ae
pedro-hos <pedro-hos@outlook.com>
2019-08-21 10:13:44 -0300

    Alterar categorias #10

--- cac-server/src/main/resources/import.sql
+++ cac-server/src/main/resources/import.sql
@@ -5,9 +5,15 @@ INSERT INTO STATUS (ID, NAME, PIN_NAME, DESCRIPTION) VALUES (3, 'Trecho perigoso
 
 
 -- INSERT TYPE --
-INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (1, 'Pintura Apagada');
-INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (2, 'Tachão solto');
-INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (3, 'Buraco');
-
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (1, 'Não tem pintura no solo');
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (2, 'Há desgaste na pintura do solo');
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (3, 'Faltam placas educativas ou alertas de segurança');
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (4, 'Buracos');
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (5, 'Piso está desnivelado ou com ondulação');
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (6, 'Bloquete/Tachão solto');
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (7, 'Tachão distante entre um e outro');
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (8, 'Trecho sem iluminação com falta de iluminação');
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (9, 'Tem mato crescendo');
+INSERT INTO OCCURRENCE_TYPE (ID, NAME) VALUES (10, 'Espaço é muito limitado');
 
 INSERT INTO SYS_USER(ID, NAME, PASSWORD, ROLE) VALUES (1, 'Penny', 'password', 'admin')
