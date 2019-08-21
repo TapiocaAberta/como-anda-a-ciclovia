48578a1366cec8819371e47ce789113b877f4b60
pedro-hos <pedro-hos@outlook.com>
2019-08-21 10:16:57 -0300

    Alterar categoria: Trecho perigoso #2

--- cac-server/src/main/resources/import.sql
+++ cac-server/src/main/resources/import.sql
@@ -1,7 +1,7 @@
 -- INSERT STATUS --
-INSERT INTO STATUS (ID, NAME, PIN_NAME, DESCRIPTION) VALUES (1, 'Ciclovia não esta bem (problemas de infra-estrurura)', 'pin-01.png', 'Description');
-INSERT INTO STATUS (ID, NAME, PIN_NAME, DESCRIPTION) VALUES (2, 'Tem reforma sendo feita (manutenção)', 'pin-02.png', 'Description 2');
-INSERT INTO STATUS (ID, NAME, PIN_NAME, DESCRIPTION) VALUES (3, 'Trecho perigoso (esse ponto merece atenção)', 'pin-02.png', 'Description 2');
+INSERT INTO STATUS (ID, NAME, PIN_NAME, DESCRIPTION) VALUES (1, 'Ciclovia não esta bem (problemas de infra-estrurura)', 'pin-01.png', '');
+INSERT INTO STATUS (ID, NAME, PIN_NAME, DESCRIPTION) VALUES (2, 'Tem reforma sendo feita (manutenção)', 'pin-02.png', '');
+INSERT INTO STATUS (ID, NAME, PIN_NAME, DESCRIPTION) VALUES (3, 'Trecho perigoso (perigo de acidentes ou falta de segurança)', 'pin-02.png', '');
 
 
 -- INSERT TYPE --
