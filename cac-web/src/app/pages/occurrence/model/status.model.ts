22f3fdcf12223f68febeac71558144dfa6c1113e
pedro-hos <pedro-hos@outlook.com>
2020-06-03 00:42:09 -0300

    removendo front para outro projeto para poder fazer deploy no heroku

--- cac-web/src/app/pages/occurrence/model/status.model.ts
+++
@@ -1,6 +0,0 @@
-export class Status {
-  id: number;
-  name: string;
-  description: string;
-  pinName: string;
-};
--- cac-web/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  resouceUrl: 'http://165.22.154.252:8080',
-  production: true,
-  iconPath: '/assets/images/'
-};
